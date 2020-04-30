3f6e1772cc8f19af178fa1708c8545724d0c6a47
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-04-30 12:21:49 -0400

    added database

--- bikes/src/main/java/com/globomatics/bikes/controllers/BikesController.java
+++ bikes/src/main/java/com/globomatics/bikes/controllers/BikesController.java
@@ -1,5 +1,29 @@
 package com.globomatics.bikes.controllers;
 
+import com.globomatics.bikes.models.Bike;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/bikes")
 public class BikesController
-{
+{   @GetMapping
+    public List<Bike> list(){
+        List<Bike> bikes = new ArrayList<>();
+        return bikes;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Bike bike){
+
+    }
+
+    @GetMapping("/{id}")
+    public Bike get(@PathVariable("id") long id){
+        return new Bike();
+    }
 }
